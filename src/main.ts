893932606363c2fbf5952207dde04905bcdbce80
injashkin <injashkin@gmail.com>
2023-03-13 10:12:18 +0300

    All the points of the task are completed

--- src/main.ts
+++ src/main.ts
@@ -2,147 +2,200 @@ import "./style.css";
 import { formatDate } from "./format-date";
 //import typescriptLogo from './typescript.svg'
 
-let submits = document.querySelector("[name='submits']");
+const comments = <HTMLElement>document.querySelector("#comments");
 
 document.addEventListener("click", handleClick);
 document.addEventListener("keydown", handleKeydown);
 
 // ========================================================
 
-function handleClick(e: Event) {
-  let textarea = document.querySelector(".textarea-wrapper");
-
-  //let { target } = e;
-  const target = <HTMLButtonElement>e.target;
-
-  const placeholder = target.querySelector<HTMLButtonElement>(".placeholder")!;
-
-  if (target === textarea) editComment(placeholder);
-}
-
 function handleKeydown(e: any) {
   const ev = e as KeyboardEvent;
   const target = e.target;
 
   if (ev.code == "Enter" && !ev.shiftKey) {
-    if (target.closest("#form")) {
-      const form = <HTMLFormElement>document.querySelector("#form");
+    if (target.closest(".comments__form")) {
+      const form = <HTMLFormElement>target.closest(".comments__form");
+      const submit = <HTMLFormElement>form.querySelector(".comments__submit");
 
-      const checkedForm = checkForm(form);
-      createComment(checkedForm);
+      if (!e.repeat) {
+        const newEvent = new Event("click", {
+          bubbles: true,
+          cancelable: true,
+        });
+        submit.dispatchEvent(newEvent);
+        e.preventDefault();
+
+        if (checkForm(form)) createComment(form);
+      }
     }
+  }
+}
+
+function handleClick(e: Event) {
+  const target = <HTMLButtonElement>e.target;
+
+  if (target.closest(".form-wrapper")) {
+    const formWrapper = <HTMLFormElement>target.closest(".form-wrapper");
+    const form = <HTMLFormElement>formWrapper.querySelector(".comments__form");
+    const appeal = <HTMLButtonElement>formWrapper.querySelector(".appeal");
+
+    if (!appeal.classList.contains("hide")) {
+      appeal.classList.add("hide");
+      openElement(form);
+    }
+  }
+
+  if (target.closest(".comments__submit")) {
+    const inputContent = <HTMLFormElement>target.closest(".input-content");
+    const appeal = <HTMLFormElement>inputContent.querySelector(".appeal");
+    const form = <HTMLFormElement>inputContent.querySelector(".comments__form");
+
+    if (!checkForm(form)) return;
+    const newComment = createComment(form);
+    inputContent.after(newComment);
+    //const checkedForm = checkForm(form);
+    // const newComment = createComment(checkedForm);
+
+    if (inputContent.closest(".children")) {
+      inputContent.remove();
+    } else {
+      closeElement(form);
+      openElement(appeal);
+    }
+  }
+
+  if (target.closest(".comment-delete")) {
+    let elem = <HTMLButtonElement>target.closest(".post-content");
+    elem.remove();
+  }
+
+  if (target.closest(".voting")) {
+    const voting = <HTMLElement>target.closest(".voting");
+    const img = <HTMLImageElement>voting.querySelector("img");
+    const count = <HTMLElement>voting.querySelector(".count");
+
+    if (img.getAttribute("src") === "/src/image/favorite.svg") {
+      img.setAttribute("src", "/src/image/favorite_fill.svg");
+      if (count.textContent)
+        count.textContent = (+count.textContent + 1).toString();
+    } else {
+      img.setAttribute("src", "/src/image/favorite.svg");
+      if (count.textContent)
+        count.textContent = (+count.textContent - 1).toString();
+    }
+  }
+
+  if (target.closest(".reply")) {
+    const inputContentList = comments.querySelectorAll(".input-content");
+    if (inputContentList.length > 1) {
+      alert("У вас уже открыта форма");
+      return;
+    }
+
+    const postWrapper = <HTMLElement>target.closest(".post-wrapper");
+    const inputContentClone = <HTMLElement>(
+      comments.querySelector(".input-content")?.cloneNode(true)
+    );
+    const children = document.createElement("div");
+    children.classList.add("children");
+    postWrapper.append(children);
+
+    children.append(inputContentClone);
+    const appeal = <HTMLFormElement>inputContentClone.querySelector(".appeal");
+    const form = <HTMLFormElement>(
+      inputContentClone.querySelector(".comments__form")
+    );
+    closeElement(appeal);
+    openElement(form);
   }
 }
 
 function createComment(form: HTMLFormElement) {
   const { user, comment, date } = form;
 
-  console.log(date.value);
-
   const { timeFormat, dateWord } = formatDate(date.value);
 
   let userName = user.value;
   let avatar = [...userName][0].toUpperCase();
-  //let date = e.target.form.date.value;
 
-  let inputContent = document.querySelector(".input-content");
   let newComment = document.createElement("div");
+
   newComment.innerHTML = `
-  <div class="post-content">
-    <div class="avatar">
-      <a href="" data-username="Влад" class="user">
-        <div>${avatar}</div>
-      </a>
-    </div>
-    <div class="post-body">
-      <header class="comment__header">
-        <span class="post-byline">
-          <span class="author publisher-anchor-color"><a href="" data-action="profile">${userName}</a></span>
-          <a class="follow-user-container"><span class="follow-user" title="Подписаться"></span></a></span>
-
-        <div class="post-meta">
-          <a href="" class="time-ago" title="Пятница, 8 Июля 2022 г., 20:13">${dateWord}, в ${timeFormat}</a>
-        </div>
-      </header>
-
-      <div class="post-message-container" data-role="message-container">
-        <div class="post-message" data-role="message">
-          <p>${comment.value}</p>
-        </div>
+  <div class="post-wrapper">
+    <div class="post-content">
+      <div class="avatar">
+        <a href="" data-username="Влад" class="user">
+          <div>${avatar}</div>
+        </a>
       </div>
+      <div class="post-body">
+        <header class="comment__header">
+          <span class="post-byline">
+            <span class="author publisher-anchor-color"><a href="" data-action="profile">${userName}</a></span>
+            <a class="follow-user-container"><span class="follow-user" title="Подписаться"></span></a></span>
 
-      <!-- ****************************** -->
+          <div class="post-meta">
+            <a href="" class="time-ago" title="Пятница, 8 Июля 2022 г., 20:13">${dateWord}, в ${timeFormat}</a>
+          </div>
+        </header>
 
-      <footer class="comment__footer">
-        <menu class="comment-footer__menu">
-          <li class="voting">
-            <div class="post-votes">
-              <a href="#" class="vote-up" title="Голосовать" name="Голосовать"><span class="control"><img
-                    src="./src/image/like.svg" /></span>
-                <span class="updatable count">0</span></a>
+        <div class="post-message-container" data-role="message-container">
+          <div class="post-message" data-role="message">
+            <p>${comment.value}</p>
+          </div>
+        </div>
+
+        <footer class="comment__footer">
+          <menu class="comment-footer__menu">
+            <div class="voting">
+              <div class="post-votes">
+                <a href="#" class="vote-up" title="Голосовать" name="Голосовать"><span class="control"><img
+                      src="/src/image/favorite.svg" /></span>
+                  <span class="updatable count">0</span></a>
+              </div>
             </div>
-          </li>
-          <li class="reply">
-            <a class="comment-footer__action" href="#" data-action="reply"><span class="text">Ответить</span></a>
-          </li>
-        </menu>
-      </footer>
+            <div class="reply">
+              <a class="comment-footer__action" href="#" data-action="reply"><span class="text">Ответить</span></a>
+            </div>
+            <div class="comment-delete">
+              <a href="#" title="Удалить комментарий"><img src="/src/image/delete.svg"/></a>
+            </div>
+          </menu>
+        </footer>
+      </div>
     </div>
   </div>
   `;
-  inputContent?.after(newComment);
+
+  return newComment;
 }
 
 function checkForm(form: HTMLFormElement) {
   const { user, comment } = form;
 
-  //if (!e.repeat) {
-  //const newEvent = new Event("click", { bubbles: true, cancelable: true });
-  //target.nextElementSibling.dispatchEvent(newEvent);
+  let isUser = true;
+  let isComment = true;
 
   if (!user.value) {
     user.placeholder = "Это поле не должно быть пустым!";
     user.style.setProperty("--placeholder-color", "red");
+    isUser = false;
   }
 
   if (!comment.value) {
     comment.placeholder = "Это поле не должно быть пустым!";
     comment.style.setProperty("--placeholder-color", "red");
+    isComment = false;
   }
 
-  //e.preventDefault();
-  // }
-  return form;
+  return isComment && isUser;
 }
 
-function editComment(placeholder: HTMLButtonElement) {
-  if (placeholder.classList.contains("hide")) return;
-  placeholder.classList.add("hide");
-
-  let form = <HTMLElement>placeholder.nextElementSibling;
-
-  //let form = document.forms.form; //querySelector(".comments__form");
-  form.classList.remove("hide");
+function openElement(elem: Element) {
+  elem.classList.remove("hide");
 }
 
-/*
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-*/
+function closeElement(elem: Element) {
+  elem.classList.add("hide");
+}
